b664ed93566f780d1ea0d8223a70962e3d9e491b
yann <yannlefranc330@gmail.com>
2023-05-06 17:25:46 +0200

    The witcher timeline

---
+++ src/thewitcher.java
@@ -0,0 +1,15 @@
+public interface thewitcher {
+    public static void main( String[] args ) {
+
+        //je me permet de récupérer le temps.
+
+        // petit bout de code pour m'excercer
+        // Récupération du temps en milliseconde
+        long timestampMS = System.currentTimeMillis();
+
+        // On affiche le timestamp.
+        System.out.printf( "Le temps avec le sorceleur : %d\n", timestampMS );
+
+    }
+
+}
